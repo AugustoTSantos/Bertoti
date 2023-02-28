a773a9b549490baa3642f0e62593864a63cf80e9
Augusto <augustotsantos@outlook.com>
2023-02-27 21:54:40 -0300

    pintado diamante

--- Padroes de Projetos de Sistemas/estrategia/Jogo.java
+++ Padroes de Projetos de Sistemas/estrategia/Jogo.java
@@ -3,11 +3,11 @@ package estrategia;
 public class Jogo {
     private Personagem personagem;
 
-    public Jogo(Personagem personagem) {
-        this.personagem = personagem;
-    }
-
     public void personagemAtacar() {
         personagem.atacar();
     }
+
+    public void setPersonagem(Personagem personagem) {
+        this.personagem = personagem;
+    }    
 }
